afe75cf29ecdea4c4b9776777dd7d71d552e11f0
muhammadasadbastomi <tomycules@gmail.com>
2021-07-18 07:29:38 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'nama' => 'admin',
+            'username' => 'admin',
+            'password' => Hash::make('123'),
+            'role' => '1',
+        ]);
+
+        DB::table('biodata_diris')->insert([
+            'user_id' => '1',
+            'NIP' => '123',
+            'alamat' => 'banjarbaru',
+            'tempat_lahir' => 'banjarbaru',
+            'tanggal_lahir' => '1998-05-06',
+            'jenis_kelamin' => '1',
+        ]);
     }
 }
- 
