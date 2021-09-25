a8c8b4518b5d07aec875ab7a9bb1561b423499b2
muhammadasadbastomi <tomycules@gmail.com>
2021-09-25 13:14:15 +0800

    add field status verification

--- database/migrations/2021_07_17_222953_create_permohonan_farmasis_table.php
+++ database/migrations/2021_07_17_222953_create_permohonan_farmasis_table.php
@@ -24,11 +24,17 @@ class CreatePermohonanFarmasisTable extends Migration
             $table->string('praktik_ke');
             $table->tinyInteger('status')->default(0);
             $table->date('verif_admin')->nullable();
+            $table->tinyInteger('status_verif_admin')->nullable();
             $table->date('verif_petugas_proses')->nullable();
+            $table->tinyInteger('status_verif_petugas_proses')->nullable();
             $table->date('verif_kasi')->nullable();
+            $table->tinyInteger('status_verif_kasi')->nullable();
             $table->date('verif_kabid')->nullable();
+            $table->tinyInteger('status_verif_kabid')->nullable();
             $table->date('verif_sekretaris')->nullable();
+            $table->tinyInteger('status_verif_sekretaris')->nullable();
             $table->date('verif_kepala_dinas')->nullable();
+            $table->tinyInteger('status_verif_kepala_dinas')->nullable();
             $table->foreign('biodata_diri_id')->references('id')->on('biodata_diris')->onDelete('cascade');
             $table->timestamps();
         });
